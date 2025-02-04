d4f7a31a2f2dfabcbc9c30a5e4085461610312bf
Maycon <mayconwiner@gmail.com>
2025-02-03 22:27:19 -0300

    inicio

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask , render_template, url_for
+
+
+app = Flask(__name__)
+
+lista_usuarios = ['Maycon','João','Beatriz','José', 'Italo','Marina','Rosimar','Ourivan','Amanda','Lucas','Rafael']
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')  
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+ 
+
+if __name__ == '__main__':
+    app.run(debug=True)
