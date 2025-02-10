b96de47bc46f0313abc73115c2c9b24d350dbc94
Maycon <mayconwiner@gmail.com>
2025-02-10 00:28:08 -0300

    relacionamento

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask , render_template, url_for, request, redirect, flash
 from forms import FormCriarConta, FormLogin
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 lista_usuarios = ['Maycon','João','Beatriz','José', 'Italo','Marina','Rosimar','Ourivan','Amanda','Lucas','Rafael']
 
 app.config['SECRET_KEY'] = 'ee23664e6e55f0d81d5f553ee19afe09'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dti.db'
 
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
---
+++ models.py
@@ -0,0 +1,25 @@
+from app import db
+from datetime import datetime
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    senha = db.Column(db.String(100), nullable=False)
+    foto_perfil = db.Column(db.String(20), nullable=False, default='default.jpg')
+    posts = db.relationship('post', backref='autor', lazy=True)
+    cursos = db.Column(db.String, default='Não informado', nullable=False)
+
+    # def __repr__(self):
+    #     return f'<Usuario {self.nome}>'
+    
+
+class post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(100), nullable=False)
+    corpo = db.Column(db.Text, nullable=False)
+    data_criacao = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'), nullable=False)
+
+    # def __repr__(self):
+    #     return f'<Post {self.titulo}>'
