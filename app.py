e89f5b7063b51c325608e44d9717faf3f729965c
Maycon <mayconwiner@gmail.com>
2025-02-09 23:15:22 -0300

    Validacao Form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template, url_for
+from flask import Flask , render_template, url_for, request, redirect, flash
 from forms import FormCriarConta, FormLogin
 
 app = Flask(__name__)
@@ -21,13 +21,22 @@ def contato():
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
-    return render_template('login.html', formLogin=form_login, formCriarConta=form_criarconta)
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
  
-
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
--- forms.py
+++ forms.py
@@ -1,18 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Email, EqualTo, length
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
 
 class FormCriarConta(FlaskForm):
-    nome = StringField('Nome', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(), length(8,120), EqualTo('confirmar_senha', message='As senhas devem ser iguais')])
-    confirmar_senha = PasswordField('Confirmar Senha', validators=[DataRequired(), length(8,120), EqualTo('senha', message='As senhas devem ser iguais')])
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criarconta = SubmitField('Criar Conta')
 
 
 class FormLogin(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(), length(8,120)])
-    lembrar_dados = BooleanField('Lembrar Dados')
-    botao_submit_login = SubmitField('Entrar')
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    lembrar_dados = BooleanField('Lembrar Dados de Acesso')
+    botao_submit_login = SubmitField('Fazer Login')
 
