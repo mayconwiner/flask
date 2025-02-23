6afe5ed45582f9d73916309e73b728e9c8b89b8f
Maycon <mayconwiner@gmail.com>
2025-02-23 16:53:01 -0300

    redirecionamento inteligente

--- teamwiner/routes.py
+++ teamwiner/routes.py
@@ -16,6 +16,7 @@ def contato():
     return render_template('contato.html')  
 
 @app.route('/usuarios')
+@login_required
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
@@ -28,7 +29,11 @@ def login():
         if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
             login_user(usuario, remember=form_login.lembrar_dados.data)
             flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
-            return redirect(url_for('home'))
+            par_next = request.args.get('next')
+            if par_next:
+                return redirect(par_next)
+            else:
+                return redirect(url_for('home'))
         else:
             flash(f'Falha no Login. E-mail ou senha incorretos', 'alert-danger')
             
